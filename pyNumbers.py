82d64df1ac6540b23a43c963d39c4ba2e57f58b1
BaconPancakez <sengqiming15@gmail.com>
2024-10-23 22:52:43 +0800

    updates

---
+++ pyNumbers.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+# array = np.random.random((3, 4))
+# print(array)
+# print(array[1, 0])
+# print(array > 0.2)
+# print(array[array > 0.2])
+# print(np.sum(array))
+# print(np.floor(array))
+# print(np.ceil(array))
+# print(np.round(array))
+
+
+dim_inch = np.array([1, 2, 3])
+dim_cm = dim_inch * 2.54
+print(dim_cm)
